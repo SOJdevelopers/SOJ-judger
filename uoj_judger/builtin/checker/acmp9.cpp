47cad8ad47178eccfca4459e4c745d2103844fc9
realskc <billshikaicheng@gmail.com>
2023-12-11 10:07:52 +0800

    add acmp2-10

---
+++ uoj_judger/builtin/checker/acmp9.cpp
@@ -0,0 +1,55 @@
+#include "testlib.h"
+#include <cmath>
+
+using namespace std;
+
+const double EPS = 1E-9;
+
+inline bool doubleAbsoluteCompare(double expected, double result, double MAX_DOUBLE_ERROR) {
+    if (__testlib_isNaN(expected)) {
+        return __testlib_isNaN(result);
+    } else if (__testlib_isInfinite(expected)) {
+        if (expected > 0) {
+            return result > 0 && __testlib_isInfinite(result);
+        } else {
+            return result < 0 && __testlib_isInfinite(result);
+        }
+    } else if (__testlib_isNaN(result) || __testlib_isInfinite(result)) {
+        return false;
+    } else if (__testlib_abs(result - expected) <= MAX_DOUBLE_ERROR + 1E-15) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+inline double doubleAbsoluteDelta(double expected, double result) {
+    double absolute = __testlib_abs(result - expected);
+    return absolute;
+}
+
+int main(int argc, char * argv[])
+{
+    setName("compare two sequences of doubles, max absolute error = %.10lf", EPS);
+    registerTestlibCmd(argc, argv);
+
+    int n = 0;
+    double j, p;
+
+    while (!ans.seekEof()) 
+    {
+        n++;
+        j = ans.readDouble();
+        p = ouf.readDouble();
+        if (!doubleAbsoluteCompare(j, p, EPS))
+        {
+            quitf(_wa, "%d%s numbers differ - expected: '%.10lf', found: '%.10lf', error = '%.10lf'",
+                n, englishEnding(n).c_str(), j, p, doubleAbsoluteDelta(j, p));
+        }
+    }
+
+    if (n == 1)
+        quitf(_ok, "found '%.10lf', expected '%.10lf', error '%.10lf'", p, j, doubleAbsoluteDelta(j, p));
+
+    quitf(_ok, "%d numbers", n);
+}
